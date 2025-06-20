b6d7769c2432d3d38093adc3ab9d5a10d160f143
shuaibola01 <shuaibolaitan01@gmail.com>
2025-06-20 10:34:23 +0100

    Delete jwt-decode.js

--- jwt-decode.js
+++
@@ -1,9 +0,0 @@
-import jwtDecode from 'jwt-decode';
-
-const token = localStorage.getItem('token');
-if (token) {
-  const decoded = jwtDecode(token);
-  if (decoded.isAdmin) {
-    navigate('/admin'); // or whatever route you use
-  }
-}
